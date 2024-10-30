881e5b984e49cfa34db921c6c094665a8c863b9c
Mark Peters <mapeters101@earthlink.net>
2024-10-30 18:52:23 -0400

    Lesson119:  Work on lesson from 3:10 up to 7:50.

--- python_3D_motion.py
+++ python_3D_motion.py
@@ -21,9 +21,32 @@ y=r*np.sin(2*np.pi*f*t)
 z=t
 
 ############################## ANIMATION ##############################
+frame_amount=len(t)
+
+def update_plot(num):
+
+    return
+
+# Set up the figure properties
+fig=plt.figure(figsize=(16,9),dpi=80,facecolor=(0.8,0.8,0.8))
+gs=gridspec.GridSpec(3,4)
+
+# 3d motion
+ax0=fig.add_subplot(gs[:,0:3],projection='3d',facecolor=(0.9,0.9,0.9))
+plane_trajectory,=ax0.plot([],[],[],'r',linewidth=4,label='Flight trajectory')
+ax0.set_xlim(min(x),max(x))
+ax0.set_ylim(min(y),max(y))
+ax0.set_zlim(min(z),max(z))
+
+ax0.set_xlabel('position_x [m]',fontsize=12)
+ax0.set_ylabel('position_y [m]',fontsize=12)
+ax0.set_xlabel('position_z [m]',fontsize=12)
+plt.grid(True)
+plt.legend(loc='upper left',fontsize='large')
 
 
 
 
 
+plt.show()
 
